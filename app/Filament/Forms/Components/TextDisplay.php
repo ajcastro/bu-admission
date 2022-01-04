0b9608c344a3b8c622c5c34bffeaf9341ffa4af8
ajcastro <ajcastro29@gmail.com>
2022-01-04 20:13:22 +0800

    Add TextDisplay view field

---
+++ app/Filament/Forms/Components/TextDisplay.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Forms\Components;
+
+use Filament\Forms\Components\ViewField;
+
+class TextDisplay extends ViewField
+{
+    protected string $view = 'filament.forms.components.text-display';
+}
---
+++ resources/views/filament/forms/components/text-display.blade.php
@@ -0,0 +1,13 @@
+<x-forms::field-wrapper
+    :id="$getId()"
+    :label="$getLabel()"
+    :label-sr-only="$isLabelHidden()"
+    :helper-text="$getHelperText()"
+    :hint="$getHint()"
+    :required="$isRequired()"
+    :state-path="$getStatePath()"
+>
+    <div x-data="{ state: $wire.entangle('{{ $getStatePath() }}') }">
+        {{ $getState() }}
+    </div>
+</x-forms::field-wrapper>
