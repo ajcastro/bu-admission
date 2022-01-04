eb3e92cc764af4facef28fe605ab66bd2eeee8f5
ajcastro <ajcastro29@gmail.com>
2022-01-04 14:30:00 +0800

    Hide FilamentInfoWidget

--- config/filament.php
+++ config/filament.php
@@ -106,7 +106,7 @@ return [
         'path' => app_path('Filament/Widgets'),
         'register' => [
             Widgets\AccountWidget::class,
-            Widgets\FilamentInfoWidget::class,
+            // Widgets\FilamentInfoWidget::class,
         ],
     ],
 
