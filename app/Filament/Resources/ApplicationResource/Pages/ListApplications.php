b42f9cc64497872fbf09bb91ae2c88ec806758df
ajcastro <ajcastro29@gmail.com>
2022-02-11 09:02:42 +0800

    Display widget NeedToVerifyEmail in application list page

--- app/Filament/Resources/ApplicationResource/Pages/ListApplications.php
+++ app/Filament/Resources/ApplicationResource/Pages/ListApplications.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\ApplicationResource\Pages;
 
 use App\Filament\Resources\ApplicationResource;
+use App\Filament\Widgets\NeedToVerifyEmail;
 use Filament\Resources\Pages\ListRecords;
 
 class ListApplications extends ListRecords
 {
     protected static string $resource = ApplicationResource::class;
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            NeedToVerifyEmail::class
+        ];
+    }
 }
--- resources/views/filament/widgets/need-to-verify-email.blade.php
+++ resources/views/filament/widgets/need-to-verify-email.blade.php
@@ -2,7 +2,7 @@
     <div class="relative h-12 flex flex-col justify-center items-center space-y-2 rtl:space-x-reverse">
         <div class="space-y-1">
             <h2 class="text-lg sm:text-xl font-bold tracking-tight">
-                Please verify your email!
+                Please verify your email first!
             </h2>
         </div>
     </div>
