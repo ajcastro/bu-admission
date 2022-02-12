fa088e9ec59256a159d4e33d4f048451d8d1f0e4
ajcastro <ajcastro29@gmail.com>
2022-02-12 19:15:56 +0800

    Save

--- app/Filament/Resources/ApplicationResource/Pages/ListApplications.php
+++ app/Filament/Resources/ApplicationResource/Pages/ListApplications.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\ApplicationResource\Pages;
 
 use App\Filament\Resources\ApplicationResource;
 use App\Filament\Widgets\NeedToVerifyEmail;
+use Filament\Pages\Actions\ButtonAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListApplications extends ListRecords
@@ -22,5 +23,28 @@ class ListApplications extends ListRecords
         return [
             NeedToVerifyEmail::class
         ];
+    }
+
+    protected function getActions(): array
+    {
+        return array_filter(array_merge(
+            [
+                $this->getExportToExcelButton(),
+            ],
+            parent::getActions(),
+        ));
+    }
+
+    private function getExportToExcelButton()
+    {
+        $user = auth()->user();
+
+        return ButtonAction::make('approve')
+            ->label('Export to Excel')
+            ->icon('heroicon-s-document-download')
+            ->color('success')
+            ->action(function () {
+            })
+            ->hidden(false);
     }
 }
