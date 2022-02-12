dd168e166fb27f1d52ddc2132cf08bd0d849d2fb
ajcastro <ajcastro29@gmail.com>
2022-02-12 23:44:50 +0800

    Add fields in Applications Export about applicant

--- app/Exports/ApplicationsExport.php
+++ app/Exports/ApplicationsExport.php
@@ -4,6 +4,7 @@ namespace App\Exports;
 
 use App\Enums\ApproverAction;
 use App\Models\Application;
+use Illuminate\Support\Carbon;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
@@ -23,12 +24,36 @@ class ApplicationsExport implements FromCollection, WithHeadings, ShouldAutoSize
     {
         return [
             'Application ID',
-            'Applicant',
+            'Last Name',
+            'First Name',
+            'Middle Name',
+            'Birthdate',
+            'Gender',
+            'Email',
+            'Mobile Number',
+            'Phone Number',
+            'Work Number',
+            'Company',
+            'Residence Address Line 1',
+            'Residence Address Line 2',
+            'Residence Municipality',
+            'Residence Province',
+            'Residence Zip Code',
+            'Residence Country',
+            'Same with Residence Address?',
+            'Permanent Address Line 1',
+            'Permanent Address Line 2',
+            'Permanent Municipality',
+            'Permanent Province',
+            'Permanent Zip Code',
+            'Permanent Country',
+
             'Program',
             'Term',
             'Status',
             'Total Units',
             'Created At',
+            'Last Updated At',
 
             'Program Adviser',
             'Endorsed?',
@@ -51,12 +76,36 @@ class ApplicationsExport implements FromCollection, WithHeadings, ShouldAutoSize
         return $this->applications->map(function (Application $item) {
             return [
                 'application_id' => $item->id,
-                'applicant' => $item->applicant_name,
+                'last_name' => $item->last_name,
+                'first_name' => $item->first_name,
+                'middle_name' => $item->middle_name,
+                'birthdate' => Carbon::parse($item->birthdate)->format('m/d/Y'),
+                'gender' => $item->gender,
+                'email' => $item->email,
+                'mobile_number' => $item->mobile_number,
+                'phone_number' => $item->phone_number,
+                'work_number' => $item->work_number,
+                'company' => $item->company,
+                'residence_address_line_1' => $item->residence_address_line_1,
+                'residence_address_line_2' => $item->residence_address_line_2,
+                'residence_municipality' => $item->residence_municipality,
+                'residence_province' => $item->residence_province,
+                'residence_zip_code' => $item->residence_zip_code,
+                'residence_country' => $item->residence_country,
+                'same_with_residence_address' => $item->same_with_residence_address,
+                'permanent_address_line_1' => $item->permanent_address_line_1,
+                'permanent_address_line_2' => $item->permanent_address_line_2,
+                'permanent_municipality' => $item->permanent_municipality,
+                'permanent_province' => $item->permanent_province,
+                'permanent_zip_code' => $item->permanent_zip_code,
+                'permanent_country' => $item->permanent_country,
+
                 'program' => $item->program->label,
                 'term' => $item->term->label,
                 'status' => $item->status,
                 'total_units' => $item->total_units,
                 'created_at' => $item->created_at->format(static::DATETIME_FORMAT),
+                'updated_at' => $item->updated_at->format(static::DATETIME_FORMAT),
 
                 'program_adviser' => ($approver = $item->getApproverByAction(ApproverAction::RECOMMEND))->user->name,
                 'endorsed' => $approver->approved_at
