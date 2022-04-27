d2f7cc18f0956c67c4676499b2a5363f6f003eba
ajcastro <ajcastro29@gmail.com>
2022-04-28 07:34:04 +0800

    Fix announcement

--- app/Filament/Widgets/Announcement.php
+++ app/Filament/Widgets/Announcement.php
@@ -17,44 +17,4 @@ class Announcement extends Widget
     {
         return true;
     }
-
-    protected function getViewData(): array
-    {
-        return [
-            'items' => $this->getItems(),
-        ];
-    }
-
-    private function getItems()
-    {
-        return collect([
-            [
-                'status' => $status = ApplicationStatus::PENDING,
-                'count' => Application::accessibleBy(auth()->user())->where('status', $status)->count(),
-                'color' => 'orange',
-            ],
-            [
-                'status' => $status = ApplicationStatus::RECOMMENDED,
-                'count' => Application::accessibleBy(auth()->user())->where('status', $status)->count(),
-                'color' => 'green',
-            ],
-            [
-                'status' => $status = ApplicationStatus::ADMITTED,
-                'count' => Application::accessibleBy(auth()->user())->where('status', $status)->count(),
-                'color' => 'green',
-            ],
-            [
-                'status' => $status = ApplicationStatus::PROCESSED,
-                'count' => Application::accessibleBy(auth()->user())->where('status', $status)->count(),
-                'color' => 'green',
-            ],
-            [
-                'status' => $status = ApplicationStatus::REJECTED,
-                'count' => Application::accessibleBy(auth()->user())->where('status', $status)->count(),
-                'color' => 'red',
-            ],
-        ])->map(function ($item) {
-            return new ApplicationSummaryItem($item['status'], $item['count'], $item['color']);
-        });
-    }
 }
--- resources/views/filament/widgets/announcement.blade.php
+++ resources/views/filament/widgets/announcement.blade.php
@@ -1,25 +1,12 @@
 <x-filament::card class="relative">
-    <div class="relative h-12 flex flex-col justify-center items-center space-y-2 rtl:space-x-reverse">
-        <div class="space-y-1">
-            <br>
-            <br>
-            <br>
-            <br>
+    <div class="relative h-12 flex flex-col items-center space-y-2 rtl:space-x-reverse">
+        <div class="space-y-2">
             <h2 class="text-lg sm:text-xl font-bold tracking-tight">
                 Announcement
             </h2>
         </div>
-        <div class="space-y-1 text-gray-600 focus:outline-none focus:underline">
-            <p>
-                {{ \App\Models\Setting::value('announcement') ?? 'Nisi nulla ea aliquip ad reprehenderit consequat fugiat nulla sit non velit eiusmod minim.' }}
-            </p>
-            <hr>
-            <p>
-                <a href="{{ \App\Models\Setting::value('google_drive') ?? 'https://drive.google.com/'}}" target="_blank">
-                    Google Drive Link:
-                    {{ \App\Models\Setting::value('google_drive') ?? 'https://drive.google.com/'}}
-                </a>
-            </p>
+        <div class="space-y-2 text-gray-600 focus:outline-none focus:underline">
+            {!! \App\Models\Setting::value('announcement') ?? '' !!}
         </div>
     </div>
 </x-filament::card>
